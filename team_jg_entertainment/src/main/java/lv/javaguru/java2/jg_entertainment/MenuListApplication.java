668f0b689cbd879b746eb79c8dcc9a3f7ef39eaf
Vija-M <vija.riga@gmailcom>
2021-09-13 10:20:09 +0300

    Menu2.1 (update)

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/MenuListApplication.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/MenuListApplication.java
@@ -7,50 +7,77 @@ import java.util.Scanner;
 public class MenuListApplication {
     public static void main(String[] args) {
         List<Menu> menus = new ArrayList<>();
-
         while (true) {
-            System.out.println("Program menu:");
-            System.out.println("1. Add menu to list");
-            System.out.println("2. Delete menu from list");
-            System.out.println("3. Show all menus in the list");
-            System.out.println("4. Exit");
-
-            System.out.println("");
-
-            System.out.println("Enter menu item number to execute:");
-            Scanner scanner = new Scanner(System.in);
-            int userChoice = Integer.parseInt(scanner.nextLine());
-
-            switch (userChoice) {
-                case 1: {
-                    System.out.println("Enter menu number: ");
-                    int number = scanner.nextInt();
-                    Menu menu = new Menu(number);
-                    menus.add(menu);
-                    System.out.println("Your menu was added to the order.");
-                    break;
-                }
-                case 2: {
-                    System.out.println("Enter menu number: ");
-                    int number = scanner.nextInt();
-                    menus.remove(new Menu(number));
-                    System.out.println("That menu was removed from the order.");
-                    break;
-                }
-                case 3: {
-                    System.out.println("Book list: ");
-                    for (Menu menu : menus) {
-                        System.out.println(menu);
-                    }
-                    System.out.println("Menu list end.");
-                    break;
-                }
-                case 4: {
-                    System.out.println("Good by!");
-                    System.exit(0);
-                }
-            }
-            System.out.println("");
+            printProgramMenu();
+            int menuNumber = getMenuNumberFromUser();
+            executeSelectedMenuItem(menus, menuNumber);
         }
+    }
+
+    private static void printProgramMenu() {
+        System.out.println("Program menu:");
+        System.out.println("1. Add menu to list");
+        System.out.println("2. Delete menu from list");
+        System.out.println("3. Show all menus in the list");
+        System.out.println("4. Exit");
+        System.out.println("");
+    }
+
+    private static int getMenuNumberFromUser() {
+        System.out.println("Enter menu item number to execute:");
+        Scanner scanner = new Scanner(System.in);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void executeSelectedMenuItem(List<Menu> menus, int selectedMenu) {
+        switch (selectedMenu) {
+            case 1: {
+                addMenuAction(menus);
+                break;
+            }
+            case 2: {
+                removeMenuAction(menus);
+                break;
+            }
+            case 3: {
+                printAllMenusAction(menus);
+                break;
+            }
+            case 4: {
+                exitProgramAction();
+                break;
+
+            }
+        }
+    }
+
+    private static void exitProgramAction() {
+        System.out.println("Good by!");
+        System.exit(0);
+    }
+
+    private static void printAllMenusAction(List<Menu> menus) {
+        System.out.println("Menu list: ");
+        for ( Menu menu : menus ) {
+            System.out.println(menu);
+        }
+        System.out.println("Menu list end.");
+    }
+
+    private static void removeMenuAction(List<Menu> menus) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter menu number: ");
+        int number = scanner.nextInt();
+        menus.remove(new Menu(number));
+        System.out.println("That menu was removed from the order.");
+    }
+
+    private static void addMenuAction(List<Menu> menus) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter menu number: ");
+        int number = scanner.nextInt();
+        Menu menu = new Menu(number);
+        menus.add(menu);
+        System.out.println("Your menu was added to the order.");
     }
 }
