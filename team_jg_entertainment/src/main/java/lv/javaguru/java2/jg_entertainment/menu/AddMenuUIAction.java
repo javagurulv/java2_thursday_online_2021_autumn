6c26c25064363e05e32a1d63f53e8d5ace2e5dc5
Vija-M <vija.riga@gmailcom>
2021-09-16 00:49:00 +0300

    DatabaseMenu2.2

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/menu/AddMenuUIAction.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/menu/AddMenuUIAction.java
@@ -1,4 +1,24 @@
 package lv.javaguru.java2.jg_entertainment.menu;
 
-public class AddMenuUIAction {
+import lv.javaguru.java2.jg_entertainment.Menu;
+
+import java.util.Scanner;
+
+public class AddMenuUIAction implements UIAction {
+
+    private DatabaseMenu database;
+
+    public AddMenuUIAction(DatabaseMenu database) {
+        this.database = database;
+    }
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter menu number: ");
+        Long number = scanner.nextLong();
+        Menu menu = new Menu(number);
+        database.save(menu);
+        System.out.println("Your menu was added to the order.");
+    }
 }
