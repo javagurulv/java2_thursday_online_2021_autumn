f370907d33147999cd28228838b386bb4d4a4d66
Vija-M <vija.riga@gmailcom>
2021-09-16 19:41:31 +0300

    Menu2

---
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/responses/menus/GetAllMenusResponse.java
@@ -0,0 +1,17 @@
+package lv.javaguru.java2.jg_entertainment.restaurant.core.responses.menus;
+
+import lv.javaguru.java2.jg_entertainment.Menu;
+
+import java.util.List;
+
+public class GetAllMenusResponse {
+    private List<Menu> menus;
+
+    public GetAllMenusResponse(List<Menu> books) {
+        this.menus = menus;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+}
---
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/responses/menus/RemoveMenuResponse.java
@@ -0,0 +1,13 @@
+package lv.javaguru.java2.jg_entertainment.restaurant.core.responses.menus;
+
+public class RemoveMenuResponse {
+    private boolean menuRemoved;
+
+    public RemoveMenuResponse(boolean menuRemoved) {
+        this.menuRemoved = menuRemoved;
+    }
+
+    public boolean isMenuRemoved() {
+        return menuRemoved;
+    }
+}
