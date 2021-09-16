978da3a0742f3109224aed7c119e22c9b3f734f4
Vija-M <vija.riga@gmailcom>
2021-09-16 20:23:41 +0300

    Menu2

---
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/services/services_menu/AddMenuService.java
@@ -0,0 +1,22 @@
+package lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_menu;
+
+import lv.javaguru.java2.jg_entertainment.Menu;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.database.DatabaseMenu;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.menus.AddMenuRequest;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.menus.AddMenuResponse;
+
+public class AddMenuService {
+
+    private DatabaseMenu databaseMenu;
+
+    public AddMenuService(DatabaseMenu databaseMenu) {
+        this.databaseMenu = databaseMenu;
+    }
+
+    public AddMenuResponse execute(AddMenuRequest request) {
+        Menu menu = new Menu (request.getTitle(), request.getDescription(), request.getPrice());
+               databaseMenu.save(menu);
+        return new AddMenuResponse(menu);
+    }
+
+}
