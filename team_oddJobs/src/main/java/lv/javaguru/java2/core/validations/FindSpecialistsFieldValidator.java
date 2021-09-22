71e85720998ee68e1e4718e96a36e3e9e0d5ed7a
Oleg <oleg.ivanov992@gmail.com>
2021-09-22 18:40:12 +0300

    lesson 5  homework (specialists classes tests refactoring)

---
+++ team_oddJobs/src/main/java/lv/javaguru/java2/core/validations/FindSpecialistsFieldValidator.java
@@ -0,0 +1,26 @@
+package lv.javaguru.java2.core.validations;
+
+import lv.javaguru.java2.core.requests.Find.FindSpecialistRequest;
+import lv.javaguru.java2.core.responce.CoreError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindSpecialistsFieldValidator {
+
+    public List<CoreError> validate(FindSpecialistRequest request) {
+        List<CoreError> errors = new ArrayList<>();
+        if (isEmpty(request.getSpecialistName()) && isEmpty(request.getSpecialistSurname()) && isEmpty(request.getSpecialistProfession())) {
+            errors.add(new CoreError("Name", "Must not be empty!"));
+            errors.add(new CoreError("Surname", "Must not be empty!"));
+            errors.add(new CoreError("Profession", "Must not be empty!"));
+        }
+        return errors;
+    }
+
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+}
