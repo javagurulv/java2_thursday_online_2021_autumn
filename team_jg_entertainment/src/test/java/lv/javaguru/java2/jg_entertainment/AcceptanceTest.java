23ec66578a501e6583be8fea573d2f041faf3fd1
Alina Pytsyk <alinap7814@gmail.com>
2021-09-26 17:38:59 +0300

    jg-restaurant test AcceptanceTestVisitors

--- team_jg_entertainment/src/test/java/lv/javaguru/java2/jg_entertainment/AcceptanceTest.java
+++ team_jg_entertainment/src/test/java/lv/javaguru/java2/jg_entertainment/AcceptanceTest.java
@@ -1,9 +1,12 @@
 package lv.javaguru.java2.jg_entertainment;
 
 import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.visitors.RequestAddVisitor;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.visitors.RequestDeleteVisitor;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.visitors.RequestShowAllVisitorsInListRestaurant;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.visitors.ResponseDeleteVisitors;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.visitors.ResponseShowAllVisitors;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_visitors.ServiceAddAllVisitors;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_visitors.ServiceDeleteVisitors;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_visitors.ServiceShowListVisitors;
 import org.junit.jupiter.api.Test;
 
@@ -18,18 +21,36 @@ class AcceptanceTest {
         RequestAddVisitor addVisitor = new RequestAddVisitor("nameZ", "surnameZ", 2569874L);
         getAddVisitorService().execute(addVisitor);
 
-        RequestAddVisitor addVisitor1 = new RequestAddVisitor("nameZ", "surnameZ",2569874L);
+        RequestAddVisitor addVisitor1 = new RequestAddVisitor("nameZ", "surnameZ", 2569874L);
         getAddVisitorService().execute(addVisitor1);
 
         ResponseShowAllVisitors response = getAllVisitorService().execute(new RequestShowAllVisitorsInListRestaurant());
         assertEquals(response.getNewVisitor().size(), 2);
     }
 
-    private ServiceAddAllVisitors getAddVisitorService(){
+    @Test
+    public void shouldDeleteVisitorFromList() {
+        RequestAddVisitor addVisitor = new RequestAddVisitor("nameZ", "surnameZ", 1L);
+        getAddVisitorService().execute(addVisitor);
+
+        RequestAddVisitor addVisitor1 = new RequestAddVisitor("nameX", "surnameX", 2L);
+        getAddVisitorService().execute(addVisitor1);
+
+        ResponseDeleteVisitors response = getDeleteVisitorService().execute(new RequestDeleteVisitor(2L, "nameX"));
+        assertTrue(response.ifIdVisitorDelete());
+    }
+
+
+
+    private ServiceAddAllVisitors getAddVisitorService() {
         return appContext.getBean(ServiceAddAllVisitors.class);
     }
 
-    private ServiceShowListVisitors getAllVisitorService(){
+    private ServiceShowListVisitors getAllVisitorService() {
         return appContext.getBean(ServiceShowListVisitors.class);
+    }
+
+    private ServiceDeleteVisitors getDeleteVisitorService() {
+        return appContext.getBean(ServiceDeleteVisitors.class);
     }
 }
