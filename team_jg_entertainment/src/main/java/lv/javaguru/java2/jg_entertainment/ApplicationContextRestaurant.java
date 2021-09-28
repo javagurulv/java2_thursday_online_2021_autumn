21801cf33aed2f47b920b649072e94979d73f484
Vija-M <vija.riga@gmailcom>
2021-09-28 20:03:04 +0300

    Menu4

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/ApplicationContextRestaurant.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/ApplicationContextRestaurant.java
@@ -1,16 +1,13 @@
 package lv.javaguru.java2.jg_entertainment;
 
-import lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_menu.AddMenuUIAction;
-import lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_menu.ExitUIAction;
-import lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_menu.GetAllMenusUIAction;
-import lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_menu.RemoveMenuUIAction;
+import lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_menu.*;
 import lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_tables.*;
 import lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_visitors.*;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.database.*;
-import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.tables.OrderingTable;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_menu.AddMenuService;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_menu.GetAllMenusService;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_menu.RemoveMenuService;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_menu.SearchMenusService;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_tables.AddTableService;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_tables.GetAllTablesService;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_tables.RemoveTableService;
@@ -21,6 +18,7 @@ import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_visi
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_visitors.ServiceShowListVisitors;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.validators.*;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.validatorsMenus.AddMenuValidator;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.services.validatorsMenus.SearchMenusRequestValidator;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.validatorsVisitors.*;
 
 import java.util.HashMap;
@@ -64,6 +62,7 @@ public class ApplicationContextRestaurant {
         beans.put(UIActionExit.class, new UIActionExit());
 //menu
         beans.put(AddMenuValidator.class, new AddMenuValidator());
+        beans.put(SearchMenusRequestValidator.class, new SearchMenusRequestValidator());
         beans.put(AddMenuService.class, new AddMenuService(getBean(DatabaseMenu.class), getBean(AddMenuValidator.class)));
         beans.put(RemoveMenuService.class, new RemoveMenuService(getBean(DatabaseMenu.class)));
         beans.put(GetAllMenusService.class, new GetAllMenusService(getBean(DatabaseMenu.class)));
@@ -71,6 +70,18 @@ public class ApplicationContextRestaurant {
         beans.put(AddMenuUIAction.class, new AddMenuUIAction(getBean(AddMenuService.class)));
         beans.put(RemoveMenuUIAction.class, new RemoveMenuUIAction(getBean(RemoveMenuService.class)));
         beans.put(GetAllMenusUIAction.class, new GetAllMenusUIAction(getBean(GetAllMenusService.class)));
+        beans.put(SearchMenusService.class, new SearchMenusService(
+                getBean(DatabaseMenu.class),
+                getBean(SearchMenusRequestValidator.class)));
+
+        beans.put(AddMenuUIAction.class, new AddMenuUIAction(
+                getBean(AddMenuService.class)));
+        beans.put(RemoveMenuUIAction.class, new RemoveMenuUIAction(
+                getBean(RemoveMenuService.class)));
+        beans.put(SearchMenusUIAction.class, new SearchMenusUIAction(
+                getBean(SearchMenusService.class)));
+        beans.put(GetAllMenusUIAction.class, new GetAllMenusUIAction(
+                getBean(GetAllMenusService.class)));
         beans.put(ExitUIAction.class, new ExitUIAction());
 //tables
         beans.put(ValidatorAddTable.class, new ValidatorAddTable());
