0750f06785dd8e4beab307c5587b8a4ff37692f2
vsavonins <viktors.savonins@proofit.lv>
2021-10-01 07:20:59 +0300

    Unit tests example

--- teacher/lesson_5_testing_with_junit_and_mockito/step_2_refactoring/src/test/java/lv/javaguru/java2/library/core/services/AddBookServiceTest.java
+++ teacher/lesson_5_testing_with_junit_and_mockito/step_2_refactoring/src/test/java/lv/javaguru/java2/library/core/services/AddBookServiceTest.java
@@ -1,17 +1,17 @@
 package lv.javaguru.java2.library.core.services;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import lv.javaguru.java2.library.core.database.Database;
@@ -30,28 +30,54 @@ public class AddBookServiceTest {
 
 	@Test
 	public void shouldReturnResponseWithErrorsWhenValidationFails() {
-		AddBookRequest request = new AddBookRequest(null, "Author");
-		List<CoreError> errors = new ArrayList<>();
-		errors.add(new CoreError("title", "Must not be empty!"));
-		Mockito.when(validator.validate(request)).thenReturn(errors);
-
-		AddBookResponse response = service.execute(request);
+		AddBookRequest notValidRequest = new AddBookRequest(null, "Author");
+		when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError("title", "Must not be empty!")));
+		AddBookResponse response = service.execute(notValidRequest);
 		assertTrue(response.hasErrors());
-		assertEquals(response.getErrors().size(), 1);
-		assertEquals(response.getErrors().get(0).getField(), "title");
-		assertEquals(response.getErrors().get(0).getMessage(), "Must not be empty!");
-
-		Mockito.verifyNoInteractions(database);
 	}
 
 	@Test
-	public void shouldAddBookToDatabase() {
-		Mockito.when(validator.validate(any())).thenReturn(new ArrayList<>());
-		AddBookRequest request = new AddBookRequest("Title", "Author");
-		AddBookResponse response = service.execute(request);
+	public void shouldReturnResponseWithErrorsReceivedFromValidator() {
+		AddBookRequest notValidRequest = new AddBookRequest(null, "Author");
+		when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError("title", "Must not be empty!")));
+		AddBookResponse response = service.execute(notValidRequest);
+		assertEquals(response.getErrors().size(), 1);
+		assertEquals(response.getErrors().get(0).getField(), "title");
+		assertEquals(response.getErrors().get(0).getMessage(), "Must not be empty!");
+	}
+
+	@Test
+	public void shouldNotInvokeDatabaseWhenRequestValidationFails() {
+		AddBookRequest notValidRequest = new AddBookRequest(null, "Author");
+		when(validator.validate(notValidRequest)).thenReturn(List.of(new CoreError("title", "Must not be empty!")));
+		service.execute(notValidRequest);
+		verifyNoInteractions(database);
+	}
+
+	@Test
+	public void shouldAddBookToDatabaseWhenRequestIsValid() {
+		AddBookRequest validRequest = new AddBookRequest("Title", "Author");
+		when(validator.validate(validRequest)).thenReturn(List.of());
+		service.execute(validRequest);
+		verify(database).save(argThat(new BookMatcher("Title", "Author")));
+	}
+
+	@Test
+	public void shouldReturnResponseWithoutErrorsWhenRequestIsValid() {
+		AddBookRequest validRequest = new AddBookRequest("Title", "Author");
+		when(validator.validate(validRequest)).thenReturn(List.of());
+		AddBookResponse response = service.execute(validRequest);
 		assertFalse(response.hasErrors());
-		Mockito.verify(database).save(
-				argThat(new BookMatcher("Title", "Author")));
+	}
+
+	@Test
+	public void shouldReturnResponseWithBookWhenRequestIsValid() {
+		AddBookRequest validRequest = new AddBookRequest("Title", "Author");
+		when(validator.validate(validRequest)).thenReturn(List.of());
+		AddBookResponse response = service.execute(validRequest);
+		assertNotNull(response.getNewBook());
+		assertEquals(response.getNewBook().getTitle(), validRequest.getTitle());
+		assertEquals(response.getNewBook().getAuthor(), validRequest.getAuthor());
 	}
 
 }
