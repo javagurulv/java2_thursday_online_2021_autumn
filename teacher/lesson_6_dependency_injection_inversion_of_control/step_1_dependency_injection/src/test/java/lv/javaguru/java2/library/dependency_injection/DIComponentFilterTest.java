7bd8cd1adf637cdb7281a820159c369b131b4d82
vsavonins <viktors.savonins@proofit.lv>
2021-10-04 10:38:34 +0300

    fix

--- teacher/lesson_6_dependency_injection_inversion_of_control/step_1_dependency_injection/src/test/java/lv/javaguru/java2/library/dependency_injection/DIComponentFilterTest.java
+++ teacher/lesson_6_dependency_injection_inversion_of_control/step_1_dependency_injection/src/test/java/lv/javaguru/java2/library/dependency_injection/DIComponentFilterTest.java
@@ -1,13 +1,10 @@
 package lv.javaguru.java2.library.dependency_injection;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
 
-@DIComponent
 public class DIComponentFilterTest {
 
 	@Test
