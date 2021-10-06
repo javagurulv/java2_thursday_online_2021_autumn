313acd93aeed20555fd66eeb5012572688764add
Julija Katukova <julija.katukova@gmail.com>
2021-10-06 17:59:04 +0300

    EditVisitServiceTest corrections

--- team_hospital/src/test/java/lv/javaguru/java2/hospital/visit/core/services/EditVisitServiceTest.java
+++ team_hospital/src/test/java/lv/javaguru/java2/hospital/visit/core/services/EditVisitServiceTest.java
@@ -1,6 +1,9 @@
 package lv.javaguru.java2.hospital.visit.core.services;
 
 import lv.javaguru.java2.hospital.database.VisitDatabaseImpl;
+import lv.javaguru.java2.hospital.domain.Doctor;
+import lv.javaguru.java2.hospital.domain.Patient;
+import lv.javaguru.java2.hospital.domain.Visit;
 import lv.javaguru.java2.hospital.visit.core.requests.EditVisitRequest;
 import lv.javaguru.java2.hospital.visit.core.responses.CoreError;
 import lv.javaguru.java2.hospital.visit.core.responses.EditVisitResponse;
@@ -15,6 +18,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,9 +27,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @RunWith(JUnitPlatform.class)
 class EditVisitServiceTest {
 
-    @Mock private VisitDatabaseImpl database;
-    @Mock private EditVisitValidator validator;
-    @InjectMocks private EditVisitService service;
+    @Mock
+    private VisitDatabaseImpl database;
+    @Mock
+    private EditVisitValidator validator;
+    @InjectMocks
+    private EditVisitService service;
 
     @Test
     public void shouldReturnErrorWhenVisitIdNotProvided() {
@@ -54,25 +61,59 @@ class EditVisitServiceTest {
         assertEquals(response.getErrors().get(0).getField(), "changes");
         assertEquals(response.getErrors().get(0).getDescription(), "Must not be empty!");
     }
-/*
+
     @Test
     public void shouldChangeDoctor() {
-
-
         EditVisitRequest request = new EditVisitRequest(1L, 1, "2");
         Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
 
+        Doctor doctor1 = new Doctor("DoctorsName1", "DoctorsSurname1", "Speciality1");
+        Patient patient = new Patient("PatientsName", "PatientsSurname", "120254-12636");
+
         List<Visit> visits = new ArrayList<>();
-        Doctor doctor = new Doctor("Name", "Surname", "Speciality");
-        Patient patient = new Patient("Name", "Surname", "120593-15634");
+        Date date = new Date(2021, 12, 21, 15, 00);
+        visits.add(new Visit(doctor1, patient, date));
+
+        Mockito.when(database.editVisit(1L, 1, "2")).thenReturn(true);
+        EditVisitResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertTrue(response.isVisitEdited());
+    }
+
+    @Test
+    public void shouldChangePatient() {
+        EditVisitRequest request = new EditVisitRequest(1L, 2, "2");
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        Doctor doctor = new Doctor("DoctorsName", "DoctorsSurname", "Speciality");
+        Patient patient1 = new Patient("PatientsName1", "PatientsSurname1", "120254-12636");
+
+        List<Visit> visits = new ArrayList<>();
+        Date date = new Date(2021, 12, 21, 15, 00);
+        visits.add(new Visit(doctor, patient1, date));
+
+        Mockito.when(database.editVisit(1L, 2, "2")).thenReturn(true);
+        EditVisitResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertTrue(response.isVisitEdited());
+    }
+
+    @Test
+    public void shouldChangeVisitDate() {
+        EditVisitRequest request = new EditVisitRequest(1L, 3, "23/12/2021 15:00");
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        Doctor doctor = new Doctor("DoctorsName", "DoctorsSurname", "Speciality");
+        Patient patient = new Patient("PatientsName", "PatientsSurname", "150254-12636");
+
+        List<Visit> visits = new ArrayList<>();
         Date date = new Date(2021, 12, 21, 15, 00);
         visits.add(new Visit(doctor, patient, date));
 
-       Long doctorsIdToChange = visits.get(0).getDoctor().getId();
-       EditVisitResponse response = service.execute(request);
-       assertFalse(response.hasErrors());
-       assertTrue(response.isVisitEdited());
-
+        Mockito.when(database.editVisit(1L, 3, "23/12/2021 15:00")).thenReturn(true);
+        EditVisitResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertTrue(response.isVisitEdited());
     }
-*/
+
 }
