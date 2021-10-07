d52883915280e3b57dd008308e5a62deb7a1ec60
Vija-M <vija.riga@gmailcom>
2021-10-07 15:44:09 +0300

    Menu5

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/requests/menus/RemoveMenuRequest.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/requests/menus/RemoveMenuRequest.java
@@ -1,6 +1,7 @@
 package lv.javaguru.java2.jg_entertainment.restaurant.core.requests.menus;
 
 public class RemoveMenuRequest {
+
     private Long menuNumberToRemove;
 
     public RemoveMenuRequest(Long menuNumberToRemove) {
