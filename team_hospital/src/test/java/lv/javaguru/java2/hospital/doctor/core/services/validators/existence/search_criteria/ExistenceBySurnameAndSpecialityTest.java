529be5c4065c57cf9ab7819b5370d7ec18d8ff56
Julija Katukova <julija.katukova@gmail.com>
2021-10-13 17:23:36 +0300

    Doctor: added tests for all DoctorExistenceBySearchCriteria Validator

---
+++ team_hospital/src/test/java/lv/javaguru/java2/hospital/doctor/core/services/validators/existence/search_criteria/ExistenceBySurnameAndSpecialityTest.java
@@ -0,0 +1,67 @@
+package lv.javaguru.java2.hospital.doctor.core.services.validators.existence.search_criteria;
+
+import lv.javaguru.java2.hospital.database.DoctorDatabaseImpl;
+import lv.javaguru.java2.hospital.doctor.core.requests.SearchDoctorsRequest;
+import lv.javaguru.java2.hospital.doctor.core.responses.CoreError;
+import lv.javaguru.java2.hospital.domain.Doctor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+class ExistenceBySurnameAndSpecialityTest {
+
+
+    @Mock
+    private DoctorDatabaseImpl database;
+    @InjectMocks
+    private ExistenceBySurnameAndSpeciality existence;
+
+    @Test
+    public void shouldReturnTrue() {
+        SearchDoctorsRequest request = new SearchDoctorsRequest(null, "", "Surname", "Speciality");
+        assertTrue(existence.canValidate(request));
+    }
+
+    @Test
+    public void shouldReturnFalse() {
+        SearchDoctorsRequest request = new SearchDoctorsRequest(null, "Name64", "Surname68543", "");
+        assertFalse(existence.canValidate(request));
+    }
+
+    @Test
+    public void shouldReturnDoctorError() {
+        SearchDoctorsRequest request = new SearchDoctorsRequest(null, "", "Surname23", "Speciality5");
+        Optional<CoreError> errorList = existence.validateExistence(request);
+        assertFalse(errorList.isEmpty());
+        assertEquals(errorList.get().getField(), "Doctor");
+        assertEquals(errorList.get().getMessage(), "Does not exist!");
+    }
+
+    @Test
+    public void shouldReturnEmptyList() {
+        Doctor doctor = new Doctor("Name253", "Surname23", "Speciality5");
+
+        SearchDoctorsRequest request = new SearchDoctorsRequest(null, "", "Surname23", "Speciality5");
+        List<Doctor> doctors = new ArrayList<>();
+        doctors.add(doctor);
+        Mockito.when(database.getDoctorsList()).thenReturn(doctors);
+
+        Optional<CoreError> errorList = existence.validateExistence(request);
+        assertTrue(errorList.isEmpty());
+    }
+
+}
