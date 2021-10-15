54107bb2a89954cc90ae976e2e793a744e17adb0
Julija Katukova <julija.katukova@gmail.com>
2021-10-15 13:58:11 +0300

    Doctor: implemented DoctorExistenceByIdValidator in EditDoctorRequestValidator

--- team_hospital/src/main/java/lv/javaguru/java2/hospital/doctor/core/services/validators/EditDoctorRequestValidator.java
+++ team_hospital/src/main/java/lv/javaguru/java2/hospital/doctor/core/services/validators/EditDoctorRequestValidator.java
@@ -1,7 +1,10 @@
 package lv.javaguru.java2.hospital.doctor.core.services.validators;
 
+import lv.javaguru.java2.hospital.doctor.core.requests.DeleteDoctorRequest;
 import lv.javaguru.java2.hospital.doctor.core.requests.EditDoctorRequest;
 import lv.javaguru.java2.hospital.doctor.core.responses.CoreError;
+import lv.javaguru.java2.hospital.doctor.core.services.validators.existence.DoctorExistenceByIdValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -11,9 +14,13 @@ import java.util.Optional;
 @Component
 public class EditDoctorRequestValidator {
 
+    @Autowired
+    private DoctorExistenceByIdValidator validator;
+
     public List<CoreError> validate(EditDoctorRequest request) {
         List<CoreError> errors = new ArrayList<>();
         validateId(request).ifPresent(errors::add);
+        validateDoctorExistence(request).ifPresent(errors::add);
         validateEditOption(request).ifPresent(errors::add);
         validateChanges(request).ifPresent(errors::add);
         return errors;
@@ -39,5 +46,12 @@ public class EditDoctorRequestValidator {
         return (request.getChanges() == null || request.getChanges().isEmpty())
                 ? Optional.of(new CoreError("changes", "Must not be empty!"))
                 : Optional.empty();
+    }
+
+    private Optional<CoreError> validateDoctorExistence(EditDoctorRequest request) {
+        if (request.getDoctorId() == null) {
+            return Optional.empty();
+        }
+        return validator.validateExistenceById(request.getDoctorId());
     }
 }
--- team_hospital/src/test/java/lv/javaguru/java2/hospital/doctor/core/services/validators/EditDoctorRequestValidatorTest.java
+++ team_hospital/src/test/java/lv/javaguru/java2/hospital/doctor/core/services/validators/EditDoctorRequestValidatorTest.java
@@ -3,20 +3,34 @@ package lv.javaguru.java2.hospital.doctor.core.services.validators;
 import lv.javaguru.java2.hospital.doctor.core.requests.EditDoctorRequest;
 import lv.javaguru.java2.hospital.doctor.core.requests.EditOption;
 import lv.javaguru.java2.hospital.doctor.core.responses.CoreError;
+import lv.javaguru.java2.hospital.doctor.core.services.validators.existence.DoctorExistenceByIdValidator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
 class EditDoctorRequestValidatorTest {
 
-    private EditDoctorRequestValidator validator = new EditDoctorRequestValidator();
+    @Mock
+    private DoctorExistenceByIdValidator existence;
+    @InjectMocks private EditDoctorRequestValidator validator;
 
     @Test
     public void shouldReturnEmptyList() {
         EditDoctorRequest request = new EditDoctorRequest(123L, EditOption.valueOf("NAME"), "changes");
+        Mockito.when(existence.validateExistenceById(123L)).thenReturn(Optional.empty());
         List<CoreError> errorList = validator.validate(request);
         assertTrue(errorList.isEmpty());
     }
@@ -34,6 +48,7 @@ class EditDoctorRequestValidatorTest {
     @Test
     public void shouldReturnChangesError() {
         EditDoctorRequest request = new EditDoctorRequest(123L, EditOption.valueOf("NAME"), "");
+        Mockito.when(existence.validateExistenceById(123L)).thenReturn(Optional.empty());
         List<CoreError> errorList = validator.validate(request);
         assertFalse(errorList.isEmpty());
         assertEquals(errorList.size(), 1);
@@ -64,4 +79,15 @@ class EditDoctorRequestValidatorTest {
         assertEquals(errorList.get(0).getMessage(), "Must contain 'NAME', 'SURNAME' or 'SPECIALITY' only!");
     }*/
 
+    @Test
+    public void shouldReturnDoctorError() {
+        EditDoctorRequest request = new EditDoctorRequest(54L, EditOption.valueOf("NAME"), "changes");
+        Mockito.when(existence.validateExistenceById(54L)).thenReturn
+                (Optional.of(new CoreError("Doctor", "Does not exist!")));
+        List<CoreError> errorList = validator.validate(request);
+        assertFalse(errorList.isEmpty());
+        assertEquals(errorList.size(), 1);
+        assertEquals(errorList.get(0).getField(), "Doctor");
+        assertEquals(errorList.get(0).getMessage(), "Does not exist!");
+    }
 }
