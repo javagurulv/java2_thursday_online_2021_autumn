236e17ea7ca973f05af022e073cf8dc9ff9ecea4
Julija Katukova <julija.katukova@gmail.com>
2021-10-22 21:07:02 +0300

    Visit: added tests for DateIsWorkingDayValidatorTest for each week day

--- team_hospital/src/test/java/lv/javaguru/java2/hospital/visit/core/services/validators/date_validator/DateIsWorkingDayValidatorTest.java
+++ team_hospital/src/test/java/lv/javaguru/java2/hospital/visit/core/services/validators/date_validator/DateIsWorkingDayValidatorTest.java
@@ -24,7 +24,7 @@ class DateIsWorkingDayValidatorTest {
     @InjectMocks private DateIsWorkingDayValidator workingDayValidator;
 
     @Test
-    public void shouldReturnDateWorkingDayError() {
+    public void shouldReturnDateWorkingDayErrorSunday() {
         String date = "16/01/2022 15:00";
         LocalDateTime localDateTime = LocalDateTime.from(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").parse(date));
         Mockito.when(getVisitDate.getVisitDateFromString(date)).thenReturn(localDateTime);
@@ -32,5 +32,61 @@ class DateIsWorkingDayValidatorTest {
         assertTrue(error.isPresent());
         assertEquals(error.get().getField(), "Date");
         assertEquals(error.get().getDescription(), "is not working day!");
+    }
+
+    @Test
+    public void shouldReturnDateWorkingDayErrorSaturday() {
+        String date = "15/01/2022 15:00";
+        LocalDateTime localDateTime = LocalDateTime.from(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").parse(date));
+        Mockito.when(getVisitDate.getVisitDateFromString(date)).thenReturn(localDateTime);
+        Optional<CoreError> error = workingDayValidator.validate(date);
+        assertTrue(error.isPresent());
+        assertEquals(error.get().getField(), "Date");
+        assertEquals(error.get().getDescription(), "is not working day!");
+    }
+
+    @Test
+    public void shouldReturnEmptyListMonday() {
+        String date = "10/01/2022 15:00";
+        LocalDateTime localDateTime = LocalDateTime.from(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").parse(date));
+        Mockito.when(getVisitDate.getVisitDateFromString(date)).thenReturn(localDateTime);
+        Optional<CoreError> error = workingDayValidator.validate(date);
+        assertTrue(error.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnEmptyListTuesday() {
+        String date = "11/01/2022 15:00";
+        LocalDateTime localDateTime = LocalDateTime.from(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").parse(date));
+        Mockito.when(getVisitDate.getVisitDateFromString(date)).thenReturn(localDateTime);
+        Optional<CoreError> error = workingDayValidator.validate(date);
+        assertTrue(error.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnEmptyListWednesday() {
+        String date = "12/01/2022 15:00";
+        LocalDateTime localDateTime = LocalDateTime.from(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").parse(date));
+        Mockito.when(getVisitDate.getVisitDateFromString(date)).thenReturn(localDateTime);
+        Optional<CoreError> error = workingDayValidator.validate(date);
+        assertTrue(error.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnEmptyListThursday() {
+        String date = "13/01/2022 15:00";
+        LocalDateTime localDateTime = LocalDateTime.from(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").parse(date));
+        Mockito.when(getVisitDate.getVisitDateFromString(date)).thenReturn(localDateTime);
+        Optional<CoreError> error = workingDayValidator.validate(date);
+        assertTrue(error.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnEmptyListFriday() {
+        String date = "14/01/2022 15:00";
+        LocalDateTime localDateTime = LocalDateTime.from(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").parse(date));
+        Mockito.when(getVisitDate.getVisitDateFromString(date)).thenReturn(localDateTime);
+        Optional<CoreError> error = workingDayValidator.validate(date);
+        assertTrue(error.isEmpty());
     }
 }
