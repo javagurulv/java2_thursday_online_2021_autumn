d1029f9321182ac4e9428ed9745fd46d11d13fa1
Oleg <oleg.ivanov992@gmail.com>
2021-10-26 20:22:27 +0300

    Acceptance tests for specialist class

--- team_oddJobs/database/dbDataInsertScript.sql
+++ team_oddJobs/database/dbDataInsertScript.sql
@@ -1,4 +1,3 @@
-
 insert into Clients (clientName,clientSurname)
 values ("clientName1","clientSurname1");
 
@@ -12,32 +11,34 @@ insert into Clients (clientId,clientName,clientSurname)
 values (10,"clientName4","clientSurname4");
 
 insert into Specialists(specialistName,specialistSurname,specialistProfession)
-values ("Name","Surname","Profession",8);
+values ("specialistName","specialistSurname","specialistProfession");
 
 insert into Specialists(specialistName,specialistSurname,specialistProfession)
-values ("Name1","Surname1","Profession1",5.5);
+values ("specialistName1","specialistSurname1","specialistProfession1");
 
 insert into Specialists(specialistName,specialistSurname,specialistProfession)
-values ("Name2","Surname2","Profession2", 2);
+values ("specialistName2","specialistSurname2","specialistProfession2");
 
 insert into Specialists(specialistName,specialistSurname,specialistProfession)
-values ("Name3","Surname3","Profession3", 6);
+values ("specialistName3","specialistSurname3","specialistProfession3");
 
 insert into Specialists(specialistId,specialistName,specialistSurname,specialistProfession)
-values (10,"Name4","Surname4","Profession4", 8.9);
+values (10,"specialistName4","specialistSurname4","specialistProfession4");
 
 insert into Advertisements (advTitle,advDescription)
 values ("title","some text");
 
-insert into Advertisements (title,description)
+insert into Advertisements (advTitle,advDescription)
 values ("title1","some text1");
 
-insert into Advertisements (title,description)
+insert into Advertisements (advTitle,advDescription)
 values ("title2","some text2");
 
-insert into Advertisements (title,description)
+insert into Advertisements (advTitle,advDescription)
 values ("title3","some text3");
 
-insert into Advertisements (id,title,description)
+insert into Advertisements (advID, advTitle,advDescription)
 values (10,"title4","some text4");
+
+
 
