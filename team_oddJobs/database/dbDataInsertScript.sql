901a811817769036cb362717d31ca6dcc163e41c
Oleg <oleg.ivanov992@gmail.com>
2021-10-26 19:31:22 +0300

    Acceptance tests for specialist class

--- team_oddJobs/database/dbDataInsertScript.sql
+++ team_oddJobs/database/dbDataInsertScript.sql
@@ -1,2 +1,43 @@
-insert into Clients(clientName,clientSurname)
-values ("Zane","Zolte");
+
+insert into Clients (clientName,clientSurname)
+values ("clientName1","clientSurname1");
+
+insert into Clients (clientName,clientSurname)
+values ("clientName2","clientSurname2");
+
+insert into Clients (clientName,clientSurname)
+values ("clientName3","clientSurname3");
+
+insert into Clients (clientId,clientName,clientSurname)
+values (10,"clientName4","clientSurname4");
+
+insert into Specialists(specialistName,specialistSurname,specialistProfession)
+values ("Name","Surname","Profession",8);
+
+insert into Specialists(specialistName,specialistSurname,specialistProfession)
+values ("Name1","Surname1","Profession1",5.5);
+
+insert into Specialists(specialistName,specialistSurname,specialistProfession)
+values ("Name2","Surname2","Profession2", 2);
+
+insert into Specialists(specialistName,specialistSurname,specialistProfession)
+values ("Name3","Surname3","Profession3", 6);
+
+insert into Specialists(specialistId,specialistName,specialistSurname,specialistProfession)
+values (10,"Name4","Surname4","Profession4", 8.9);
+
+insert into Advertisements (advTitle,advDescription)
+values ("title","some text");
+
+insert into Advertisements (title,description)
+values ("title1","some text1");
+
+insert into Advertisements (title,description)
+values ("title2","some text2");
+
+insert into Advertisements (title,description)
+values ("title3","some text3");
+
+insert into Advertisements (id,title,description)
+values (10,"title4","some text4");
+
