2c1a02063c431b0f8e8622f56e06db633033e8d1
Vija-M <vija.riga@gmailcom>
2021-10-28 18:46:41 +0300

    DBupdate

--- team_jg_entertainment/database/dbDataInsertScript.sql
+++ team_jg_entertainment/database/dbDataInsertScript.sql
@@ -1,32 +1,40 @@
 insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
-values ("Name1", "Surname1", "3721")
+values ("Name1", "Surname1", "3721");
 
 insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
-values ("Name2", "Surname2", "3722")
+values ("Name2", "Surname2", "3722");
 
 insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
-values ("Name3", "Surname3", "3723")
+values ("Name3", "Surname3", "3723");
 
 insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
-values ("Name4", "Surname4", "3724")
+values ("Name4", "Surname4", "3724");
 
 insert into jg_entertainment.visitors(visitor_id, visitor_name, visitor_surname, visitor_telephone_number)
-values (1005, "Name5", "Surname5", "3725")
+values (1005, "Name5", "Surname5", "3725");
 
 insert into jg_entertainment.visitors(visitor_id, visitor_name, visitor_surname, visitor_telephone_number)
-values (1006, "Name6", "Surname6", "3726")
+values (1006, "Name6", "Surname6", "3726");
 
 insert into jg_entertainment.visitors(visitor_id, visitor_name, visitor_surname, visitor_telephone_number)
-values (1007, "Name7", "Surname7", "3727")
+values (1007, "Name7", "Surname7", "3727");
 
 
 insert into jg_entertainment.menu(menu_title, menu_description, menu_price)
-values ("title1", "description1", "10")
+values ("title1", "description1", "10");
 
 insert into jg_entertainment.menu(menu_title, menu_description, menu_price)
-values ("title2", "description2", "20")
+values ("title2", "description2", "20");
 
 insert into jg_entertainment.menu(menu_id, menu_title, menu_description, menu_price)
-values (1003, "title4", "description4", "40")
+values (1003, "title4", "description4", "30");
 
 
+insert into jg_entertainment.tables(table_title, table_capacity, table_price)
+values ("title1", "4", "80");
+
+insert into jg_entertainment.tables(table_title, table_capacity, table_price)
+values ("title2", "4", "80");
+
+insert into jg_entertainment.tables(table_title, table_capacity, table_price)
+values ("title3", "6", "110");
