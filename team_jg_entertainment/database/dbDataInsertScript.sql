9731663080b2c10447ebec9ff24dc0d2dd0d4816
Alina Pytsyk <alinap7814@gmail.com>
2021-10-28 17:24:52 +0300

    jg-restaurant: insertScript

---
+++ team_jg_entertainment/database/dbDataInsertScript.sql
@@ -0,0 +1,30 @@
+insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
+values ("Name1", "Surname1", "3721")
+
+insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
+values ("Name2", "Surname2", "3722")
+
+insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
+values ("Name3", "Surname3", "3723")
+
+insert into jg_entertainment.visitors(visitor_name, visitor_surname, visitor_telephone_number)
+values ("Name4", "Surname4", "3724")
+
+insert into jg_entertainment.visitors(visitor_id, visitor_name, visitor_surname, visitor_telephone_number)
+values (1005, "Name5", "Surname5", "3725")
+
+
+insert into jg_entertainment.menu(menu_title, menu_description, menu_price)
+values ("title1", "description1", "10")
+
+insert into jg_entertainment.menu(menu_title, menu_description, menu_price)
+values ("title2", "description2", "20")
+
+insert into jg_entertainment.menu(menu_title, menu_description, menu_price)
+values ("title3", "description3", "30")
+
+insert into jg_entertainment.menu(menu_id, menu_title, menu_description, menu_price)
+values (1004, "title4", "description4", "40")
+
+insert into jg_entertainment.menu(menu_id, menu_title, menu_description, menu_price)
+values (1005, "title5", "description5", "50")
