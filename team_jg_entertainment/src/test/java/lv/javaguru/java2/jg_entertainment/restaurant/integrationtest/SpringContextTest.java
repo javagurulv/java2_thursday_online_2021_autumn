ee021d26ca8ec4a1e52bae0e8f75c018aaa23dc0
Alina Pytsyk <alinap7814@gmail.com>
2021-11-07 10:51:59 +0200

    jg-restaurant:

--- team_jg_entertainment/src/test/java/lv/javaguru/java2/jg_entertainment/restaurant/integrationtest/SpringContextTest.java
+++ team_jg_entertainment/src/test/java/lv/javaguru/java2/jg_entertainment/restaurant/integrationtest/SpringContextTest.java
@@ -1,24 +1,15 @@
 package lv.javaguru.java2.jg_entertainment.restaurant.integrationtest;
 
-import lv.javaguru.java2.jg_entertainment.restaurant.configuration.RestaurantListConfiguration;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.assertNotNull;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {RestaurantListConfiguration.class})
-public class SpringContextTest {
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Test
-    public void start() {
-        assertNotNull(applicationContext);
-    }
-}
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {RestaurantListConfiguration.class})
+//public class SpringContextTest {
+//
+//    @Autowired
+//    private ApplicationContext applicationContext;
+//
+//    @Test
+//    public void start() {
+//        assertNotNull(applicationContext);
+//    }
+//}
