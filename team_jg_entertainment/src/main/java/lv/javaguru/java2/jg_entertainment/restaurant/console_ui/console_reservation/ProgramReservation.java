920e2e186a074cddc8d10101c0be7a3e3d60f1b5
Vija-M <vija.riga@gmailcom>
2021-11-11 18:22:23 +0200

    Menu update

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/console_ui/console_reservation/ProgramReservation.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/console_ui/console_reservation/ProgramReservation.java
@@ -11,6 +11,7 @@ import java.util.Map;
 @Component
 public class ProgramReservation {
 
+
     private Map<Integer, ReservationUIAction> menuNumberToUIActionMap;
     private CheckMenuNumberFromConsole checkNumberFromConsole = new CheckMenuNumberFromConsole();
 
