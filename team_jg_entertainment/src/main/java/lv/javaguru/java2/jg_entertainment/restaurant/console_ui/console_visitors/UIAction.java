5963e1d6cdb82ebdfe46a6879152e666a63e3eb9
Alina Pytsyk <alinap7814@gmail.com>
2021-11-11 19:30:29 +0200

    jg-restaurant:

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/console_ui/console_visitors/UIAction.java
+++
@@ -1,8 +0,0 @@
-package lv.javaguru.java2.jg_entertainment.restaurant.console_ui.console_visitors;
-
-import org.springframework.context.ApplicationContext;
-
-public interface UIAction {
-
-    void execute(ApplicationContext applicationContext);
-}
