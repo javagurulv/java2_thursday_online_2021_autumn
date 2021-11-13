fcb0316cdb8a0d02da936db6c1b35f9a5783aa49
Alina Pytsyk <alinap7814@gmail.com>
2021-11-13 19:13:16 +0200

    jg-restaurant:test

--- team_jg_entertainment/src/test/java/lv/javaguru/java2/jg_entertainment/restaurant/dependency_injection/DIComponentCreatorTest.java
+++ team_jg_entertainment/src/test/java/lv/javaguru/java2/jg_entertainment/restaurant/dependency_injection/DIComponentCreatorTest.java
@@ -1,17 +1,21 @@
 package lv.javaguru.java2.jg_entertainment.restaurant.dependency_injection;
 
-//
-//class DIComponentCreatorTest {
-//
-//    @Test
-//    public void test() throws IOException, ClassNotFoundException {
-//        ClassFinder classFinder = new ClassFinder();
-//        DIComponentFilter filter = new DIComponentFilter();
-//        ApplicationContext appContext = new ApplicationContext();
-//        DIComponentCreator creator = new DIComponentCreator();
-//        List<Class> classes = classFinder.findClassesInsidePackage("lv.javaguru.java2.jg_entertainment.restaurant");
-//        List<Class> diComponent = filter.filter(classes);
-//        diComponent.forEach(aClass -> System.out.println(aClass.getName()));
-//        creator.create(appContext, diComponent);
-//    }
-//}
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+class DIComponentCreatorTest {
+
+    @Test
+    public void test() throws IOException, ClassNotFoundException {
+        ClassFinder classFinder = new ClassFinder();
+        DIComponentFilter filter = new DIComponentFilter();
+        ApplicationContext appContext = new ApplicationContext();
+        DIComponentCreator creator = new DIComponentCreator();
+        List<Class> classes = classFinder.findClassesInsidePackage("lv.javaguru.java2.jg_entertainment.restaurant");
+        List<Class> diComponent = filter.filter(classes);
+        diComponent.forEach(aClass -> System.out.println(aClass.getName()));
+        creator.create(appContext, diComponent);
+    }
+}
