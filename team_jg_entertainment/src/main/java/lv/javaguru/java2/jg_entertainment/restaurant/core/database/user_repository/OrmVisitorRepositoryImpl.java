07bc8d760dcf87819a24b78bc4cda576b87cb87d
Alina Pytsyk <alinap7814@gmail.com>
2021-11-13 21:33:41 +0200

    jg-restaurant:

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/database/user_repository/OrmVisitorRepositoryImpl.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/database/user_repository/OrmVisitorRepositoryImpl.java
@@ -4,8 +4,8 @@ import lv.javaguru.java2.jg_entertainment.restaurant.domain.Visitors;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 //@Component
@@ -22,7 +22,7 @@ public class OrmVisitorRepositoryImpl implements VisitorsRepository {
     @Override
     public List<Visitors> findVisitorsByNameAndTelephoneNumber(String nameVisitors, String telephoneNumber) {
         Query query = sessionFactory.getCurrentSession().createQuery(
-                "SELECT v FROM visitors v WHERE visitor_name = :visitor_name AND visitor_telephone_number = :visitor_telephone_number");
+                "SELECT v FROM Visitors v WHERE visitor_name = :visitor_name AND visitor_telephone_number = :visitor_telephone_number");
         query.setParameter("visitor_name", nameVisitors);
         query.setParameter("visitor_telephone_number", telephoneNumber);
         return query.getResultList();
@@ -31,7 +31,7 @@ public class OrmVisitorRepositoryImpl implements VisitorsRepository {
     @Override
     public List<Visitors> findClientById(Long idVisitors) {
         Query query = sessionFactory.getCurrentSession()
-                .createQuery("SELECT v FROM visitors v WHERE visitor_id = :visitor_id");
+                .createQuery("SELECT v FROM Visitors v WHERE visitor_id = :visitor_id");
         query.setParameter("visitor_id", idVisitors);
         return query.getResultList();
     }
@@ -39,7 +39,7 @@ public class OrmVisitorRepositoryImpl implements VisitorsRepository {
     @Override
     public List<Visitors> findByNameVisitor(String nameVisitor) {
         Query query = sessionFactory.getCurrentSession()
-                .createQuery("SELECT v FROM visitors v WHERE visitor_name = :visitor_name");
+                .createQuery("SELECT v FROM Visitors v WHERE visitor_name = :visitor_name");
         query.setParameter("visitor_name", nameVisitor);
         return query.getResultList();
     }
@@ -47,7 +47,7 @@ public class OrmVisitorRepositoryImpl implements VisitorsRepository {
     @Override
     public List<Visitors> findBySurnameVisitor(String surnameVisitor) {
         Query query = sessionFactory.getCurrentSession()
-                .createQuery("SELECT v FROM visitors v WHERE visitor_surname = :visitor_surname");
+                .createQuery("SELECT v FROM Visitors v WHERE visitor_surname = :visitor_surname");
         query.setParameter("visitor_name", surnameVisitor);
         return query.getResultList();
     }
@@ -55,7 +55,7 @@ public class OrmVisitorRepositoryImpl implements VisitorsRepository {
     @Override
     public List<Visitors> findByNameAndSurname(String nameVisitor, String surnameVisitor) {
         Query query = sessionFactory.getCurrentSession().createQuery(
-                "SELECT v FROM visitors v WHERE visitor_name = :visitor_name AND visitor_surname = :visitor_surname");
+                "SELECT v FROM Visitors v WHERE visitor_name = :visitor_name AND visitor_surname = :visitor_surname");
         query.setParameter("visitor_name", nameVisitor);
         query.setParameter("visitor_surname", surnameVisitor);
         return query.getResultList();
@@ -64,7 +64,7 @@ public class OrmVisitorRepositoryImpl implements VisitorsRepository {
     @Override
     public boolean deleteClientWithIDAndName(Long id, String nameVisitors) {
         Query query = sessionFactory.getCurrentSession()
-                .createQuery("DELETE visitors where visitor_id = :visitor_id AND visitor_name = :visitor_name");
+                .createQuery("DELETE Visitors WHERE visitor_id = :visitor_id AND visitor_name = :visitor_name");
         query.setParameter("visitor_id", id);
         query.setParameter("visitor_name", nameVisitors);
         int result = query.executeUpdate();
@@ -74,7 +74,7 @@ public class OrmVisitorRepositoryImpl implements VisitorsRepository {
     @Override
     public List<Visitors> showAllClientsInList() {
         return sessionFactory.getCurrentSession()
-                .createQuery("SELECT v FROM visitors v", Visitors.class)
+                .createQuery("SELECT v FROM Visitors v", Visitors.class)
                 .getResultList();
     }
 }
