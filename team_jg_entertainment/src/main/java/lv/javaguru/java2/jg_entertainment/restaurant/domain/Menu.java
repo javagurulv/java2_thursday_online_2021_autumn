2c1c7121b7724dfb136c98e71514865ea66d11c2
Alina Pytsyk <alinap7814@gmail.com>
2021-11-14 14:38:08 +0200

    jg-restaurant:

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/domain/Menu.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/domain/Menu.java
@@ -1,6 +1,10 @@
 package lv.javaguru.java2.jg_entertainment.restaurant.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Objects;
 
