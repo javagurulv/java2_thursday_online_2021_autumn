24ff4ca83c87d06c9bcd1ea63181326eaa7cbdbb
Alina Pytsyk <alinap7814@gmail.com>
2021-11-19 19:38:50 +0200

    jg-restaurant:testGetReservation

---
+++ team_jg_entertainment/src/test/java/lv/javaguru/java2/jg_entertainment/restaurant/core/services/services_reservation/ShowReservationServiceTest.java
@@ -0,0 +1,52 @@
+package lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_reservation;
+
+import lv.javaguru.java2.jg_entertainment.restaurant.core.database.reservation_repository.ReservationRepository;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.reservation.ShowReservationRequest;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.reservations.ShowReservationResponse;
+import lv.javaguru.java2.jg_entertainment.restaurant.domain.Menu;
+import lv.javaguru.java2.jg_entertainment.restaurant.domain.Reservation;
+import lv.javaguru.java2.jg_entertainment.restaurant.domain.Table;
+import lv.javaguru.java2.jg_entertainment.restaurant.domain.Visitor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ShowReservationServiceTest {
+
+    @Mock private ReservationRepository databaseReservation;
+    @InjectMocks private ShowReservationService service;
+
+    @Test
+    public void shouldShowListReservation(){
+        List<Reservation> reservations = new ArrayList<>();
+        Visitor visitor = new Visitor("name", "surname", "3723");
+        visitor.setIdClient(10L);
+        Menu menu = new Menu("menu", "description", 10.00);
+        menu.setNumber(30L);
+        Table table = new Table("title", 1, 10.00);
+        table.setId(20L);
+        LocalDateTime dateTime = LocalDateTime.from((DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm").parse("20-11-2021 19:00")));
+        reservations.add(new Reservation(visitor, menu, table, dateTime));
+        Mockito.when(databaseReservation.getAllReservations()).thenReturn(reservations);
+        ShowReservationRequest request = new ShowReservationRequest();
+        ShowReservationResponse response = service.execute(request);
+        assertFalse(response.hasError());
+        assertEquals(response.getReservations().size(), 1);
+        assertEquals(response.getReservations().get(0).getVisitor(), visitor);
+        assertEquals(response.getReservations().get(0).getMenu(), menu);
+        assertEquals(response.getReservations().get(0).getTable(), table);
+        assertEquals(response.getReservations().get(0).getReservationDate(), dateTime);
+    }
+}
