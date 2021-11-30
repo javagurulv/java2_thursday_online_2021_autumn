208c5a9a73af35c42d58dabe3295c65b457846a3
Vija-M <vija.riga@gmailcom>
2021-11-30 19:53:13 +0200

    Menu update

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/web_ui/controllers/user/AddUserController.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/web_ui/controllers/user/AddUserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class AddController {
+public class AddUserController {
 
     @Autowired private AddAllUsersService service;
 
