5febeacbeef8697702104185b1662a8135fa45c8
Vija-M <vija.riga@gmailcom>
2021-11-30 19:29:06 +0200

    Menu update

---
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/web_ui/controllers/table/AddTableController.java
@@ -0,0 +1,36 @@
+package lv.javaguru.java2.jg_entertainment.restaurant.web_ui.controllers.table;
+
+import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.tables.AddTableRequest;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.tables.AddTableResponse;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_tables.AddTableService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AddTableController {
+
+    @Autowired
+    private AddTableService service;
+
+    @GetMapping(value = "/addTable")
+    public String showAddTablePage(ModelMap modelMap) {
+        modelMap.addAttribute("request", new AddTableRequest());
+        return "table/addTable";
+    }
+
+    @PostMapping("/addTable")
+    public String processAddTableRequest(@ModelAttribute(value = "request") AddTableRequest request, ModelMap modelMap) {
+        AddTableResponse response = service.execute(request);
+        if (response.hasError()) {
+            modelMap.addAttribute("errors", response.getErrorsList());
+            return "table/addTable";
+        } else {
+            return "redirect:/";
+        }
+    }
+}
+
