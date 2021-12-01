edda7d79b638bce16128f1685f363b0f2b1b80b0
Alina Pytsyk <alinap7814@gmail.com>
2021-12-02 01:14:54 +0200

    jg-restaurant:html reservation

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/requests/reservation/AddReservationRequest.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/core/requests/reservation/AddReservationRequest.java
@@ -14,6 +14,9 @@ public class AddReservationRequest {
         this.reservationDate = reservationDate;
     }
 
+    public AddReservationRequest() {
+    }
+
     public String getUserID() {
         return userID;
     }
@@ -28,5 +31,21 @@ public class AddReservationRequest {
 
     public String getReservationDate() {
         return reservationDate;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setMenuID(String menuID) {
+        this.menuID = menuID;
+    }
+
+    public void setTableID(String tableID) {
+        this.tableID = tableID;
+    }
+
+    public void setReservationDate(String reservationDate) {
+        this.reservationDate = reservationDate;
     }
 }
---
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/web_ui/controllers/reservation/AddReservationController.java
@@ -0,0 +1,34 @@
+package lv.javaguru.java2.jg_entertainment.restaurant.web_ui.controllers.reservation;
+
+import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.reservation.AddReservationRequest;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.reservations.AddReservationResponse;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_reservation.AddReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AddReservationController {
+
+    @Autowired private AddReservationService service;
+
+    @GetMapping(value = "/addReservation")
+    public String showAddUserPage(ModelMap modelMap) {
+        modelMap.addAttribute("request", new AddReservationRequest());
+        return "reservation/addReservation";
+    }
+    @PostMapping("/addReservation")
+    public String processAddUserRequest(@ModelAttribute(value = "request")
+                                                    AddReservationRequest request, ModelMap modelMap) {
+        AddReservationResponse response = service.execute(request);
+        if (response.hasError()) {
+            modelMap.addAttribute("errors", response.getErrorList());
+            return "reservation/addReservation";
+        } else {
+            return "redirect:/";
+        }
+    }
+}
