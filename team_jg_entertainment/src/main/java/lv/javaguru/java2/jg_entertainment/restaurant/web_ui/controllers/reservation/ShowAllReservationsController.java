e0b2b30ca378c852ee398ead3856bf871b6afabd
Vija-M <vija.riga@gmailcom>
2021-12-02 20:35:49 +0200

    Menu update

---
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/web_ui/controllers/reservation/ShowAllReservationsController.java
@@ -0,0 +1,24 @@
+package lv.javaguru.java2.jg_entertainment.restaurant.web_ui.controllers.reservation;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.reservation.ShowReservationRequest;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.reservations.ShowReservationResponse;
+import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_reservation.ShowReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShowAllReservationsController {
+/*
+        @Autowired
+        private ShowReservationService service;
+
+        @GetMapping(value = "/showAllReservations")
+        public String showAllReservations(ModelMap modelMap) {
+            ShowReservationResponse response = service.execute(new ShowReservationRequest());
+            modelMap.addAttribute("reservation", response.getReservations());
+            return "reservation/showAllReservations";
+        }
+
+ */
+    }
