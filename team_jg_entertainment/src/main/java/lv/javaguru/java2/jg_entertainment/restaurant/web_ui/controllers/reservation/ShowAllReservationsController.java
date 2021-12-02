8cee424905e886737562eea9e45ff07441619c06
Alina Pytsyk <alinap7814@gmail.com>
2021-12-02 21:24:42 +0200

    jg-restaurant:html reservation

--- team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/web_ui/controllers/reservation/ShowAllReservationsController.java
+++ team_jg_entertainment/src/main/java/lv/javaguru/java2/jg_entertainment/restaurant/web_ui/controllers/reservation/ShowAllReservationsController.java
@@ -1,4 +1,5 @@
 package lv.javaguru.java2.jg_entertainment.restaurant.web_ui.controllers.reservation;
+
 import lv.javaguru.java2.jg_entertainment.restaurant.core.requests.reservation.ShowReservationRequest;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.responses.reservations.ShowReservationResponse;
 import lv.javaguru.java2.jg_entertainment.restaurant.core.services.services_reservation.ShowReservationService;
@@ -9,16 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShowAllReservationsController {
-/*
-        @Autowired
-        private ShowReservationService service;
 
-        @GetMapping(value = "/showAllReservations")
-        public String showAllReservations(ModelMap modelMap) {
-            ShowReservationResponse response = service.execute(new ShowReservationRequest());
-            modelMap.addAttribute("reservation", response.getReservations());
-            return "reservation/showAllReservations";
-        }
+    @Autowired private ShowReservationService service;
 
- */
+    @GetMapping(value = "/showAllReservations")
+    public String showAllReservations(ModelMap modelMap) {
+        ShowReservationResponse response = service.execute(new ShowReservationRequest());
+        modelMap.addAttribute("reservations", response.getReservations());
+        return "reservation/showAllReservations";
     }
+}
