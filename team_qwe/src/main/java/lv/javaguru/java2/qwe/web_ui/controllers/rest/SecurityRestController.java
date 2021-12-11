9216b7348dd6832f5d0878445f30d50d69d54a84
alex <kalashnikov_alex@yahoo.com>
2021-12-11 23:56:29 +0200

    qwe finance project - REST API added for AddStockService;

--- team_qwe/src/main/java/lv/javaguru/java2/qwe/web_ui/controllers/rest/SecurityRestController.java
+++ team_qwe/src/main/java/lv/javaguru/java2/qwe/web_ui/controllers/rest/SecurityRestController.java
@@ -1,23 +1,31 @@
 package lv.javaguru.java2.qwe.web_ui.controllers.rest;
 
+import lv.javaguru.java2.qwe.core.requests.data_requests.AddStockRequest;
 import lv.javaguru.java2.qwe.core.requests.data_requests.FindSecurityByTickerOrNameRequest;
+import lv.javaguru.java2.qwe.core.responses.data_responses.AddStockResponse;
 import lv.javaguru.java2.qwe.core.responses.data_responses.FindSecurityByTickerOrNameResponse;
+import lv.javaguru.java2.qwe.core.services.data_services.AddStockService;
 import lv.javaguru.java2.qwe.core.services.data_services.FindSecurityByTickerOrNameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/security")
 public class SecurityRestController {
 
     @Autowired private FindSecurityByTickerOrNameService service;
+    @Autowired private AddStockService addStockService;
 
     @GetMapping(path = "/{ticker}", produces = "application/json")
     public FindSecurityByTickerOrNameResponse getSecurity(@PathVariable String ticker) {
         return service.execute(new FindSecurityByTickerOrNameRequest(ticker));
+    }
+
+    @PostMapping(path = "/",
+            consumes = "application/json",
+            produces = "application/json")
+    public AddStockResponse addStock(@RequestBody AddStockRequest request) {
+        return addStockService.execute(request);
     }
 
 }
