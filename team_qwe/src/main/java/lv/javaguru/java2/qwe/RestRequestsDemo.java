ccb6b74e7c5c36cfd5f0bf46f84c0d5c6a7b57e9
alex <kalashnikov_alex@yahoo.com>
2021-12-12 14:42:00 +0200

    qwe finance project - REST API added for RemoveStockService; - RestTemplate test application added;

---
+++ team_qwe/src/main/java/lv/javaguru/java2/qwe/RestRequestsDemo.java
@@ -0,0 +1,28 @@
+package lv.javaguru.java2.qwe;
+
+import lv.javaguru.java2.qwe.core.domain.Stock;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class RestRequestsDemo {
+
+    public static void main(String[] args) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        //найти акцию Apple
+        String url = "http://localhost:8080/security/{ticker}";
+        Stock stock = restTemplate.getForObject(url, Stock.class, "AAPL");
+        System.out.println(stock);
+
+        //Добавить акцию Alibaba
+        Stock stock1 = new Stock("BABA", "Alibaba", "Technology", "USD", 125.23, 0, 1.32);
+        String url1 = "http://localhost:8080/security/";
+        ResponseEntity<Stock> response = restTemplate.postForEntity(url1, stock1, Stock.class);
+        System.out.println(response.getBody());
+
+        //Удалить акцию Alibaba
+        restTemplate.delete(url, "BABA");
+    }
+
+}
--- team_qwe/src/main/java/lv/javaguru/java2/qwe/web_ui/controllers/rest/SecurityRestController.java
+++ team_qwe/src/main/java/lv/javaguru/java2/qwe/web_ui/controllers/rest/SecurityRestController.java
@@ -1,11 +1,14 @@
 package lv.javaguru.java2.qwe.web_ui.controllers.rest;
 
+import lv.javaguru.java2.qwe.core.domain.Security;
+import lv.javaguru.java2.qwe.core.domain.Stock;
 import lv.javaguru.java2.qwe.core.requests.data_requests.AddStockRequest;
 import lv.javaguru.java2.qwe.core.requests.data_requests.FindSecurityByTickerOrNameRequest;
-import lv.javaguru.java2.qwe.core.responses.data_responses.AddStockResponse;
-import lv.javaguru.java2.qwe.core.responses.data_responses.FindSecurityByTickerOrNameResponse;
+import lv.javaguru.java2.qwe.core.requests.data_requests.RemoveSecurityRequest;
+import lv.javaguru.java2.qwe.core.responses.data_responses.RemoveSecurityResponse;
 import lv.javaguru.java2.qwe.core.services.data_services.AddStockService;
 import lv.javaguru.java2.qwe.core.services.data_services.FindSecurityByTickerOrNameService;
+import lv.javaguru.java2.qwe.core.services.data_services.RemoveSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,19 +16,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/security")
 public class SecurityRestController {
 
-    @Autowired private FindSecurityByTickerOrNameService service;
+    @Autowired private FindSecurityByTickerOrNameService findSecurityService;
     @Autowired private AddStockService addStockService;
+    @Autowired private RemoveSecurityService removeSecurityService;
 
     @GetMapping(path = "/{ticker}", produces = "application/json")
-    public FindSecurityByTickerOrNameResponse getSecurity(@PathVariable String ticker) {
-        return service.execute(new FindSecurityByTickerOrNameRequest(ticker));
+    public Security getSecurity(@PathVariable String ticker) {
+        return findSecurityService.execute(new FindSecurityByTickerOrNameRequest(ticker)).getSecurity();
     }
 
     @PostMapping(path = "/",
             consumes = "application/json",
             produces = "application/json")
-    public AddStockResponse addStock(@RequestBody AddStockRequest request) {
-        return addStockService.execute(request);
+    public Stock addStock(@RequestBody AddStockRequest request) {
+        return addStockService.execute(request).getNewStock();
+    }
+
+    @DeleteMapping(path = "/{ticker}", produces = "application/json")
+    public boolean removeStock(@PathVariable String ticker) {
+        return removeSecurityService.execute(new RemoveSecurityRequest(ticker)).isRemoved();
     }
 
 }
