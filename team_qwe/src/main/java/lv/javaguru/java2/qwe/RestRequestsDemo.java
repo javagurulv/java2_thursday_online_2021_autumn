8da7efd000ee5bdc5dc0b32d0c962a618c6029ef
alex <kalashnikov_alex@yahoo.com>
2021-12-12 20:49:44 +0200

    qwe finance project - REST API added for GetUserPortfolioService;

--- team_qwe/src/main/java/lv/javaguru/java2/qwe/RestRequestsDemo.java
+++ team_qwe/src/main/java/lv/javaguru/java2/qwe/RestRequestsDemo.java
@@ -5,30 +5,39 @@ import lv.javaguru.java2.qwe.core.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 public class RestRequestsDemo {
 
     public static void main(String[] args) {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        //найти акцию Apple
+        //найти акцию
         String url = "http://localhost:8080/security/{ticker}";
         Stock stock = restTemplate.getForObject(url, Stock.class, "AAPL");
         System.out.println(stock);
 
-        //Добавить акцию Alibaba
+        //Добавить акцию
         Stock stock1 = new Stock("BABA", "Alibaba", "Technology", "USD", 125.23, 0, 1.32);
         String url1 = "http://localhost:8080/security/";
         ResponseEntity<Stock> response = restTemplate.postForEntity(url1, stock1, Stock.class);
         System.out.println(response.getBody());
 
-        //Удалить акцию Alibaba
+        //Удалить акцию
         restTemplate.delete(url, "BABA");
 
-        //найти пользователя (сложный класс, bidirectional relation)
+        //найти пользователя
         String url2 = "http://localhost:8080/user/{name}";
-        User user = restTemplate.getForObject(url2, User.class, "Alexander");
+        User user = restTemplate.getForObject(url2, User.class, "Vladimir");
         System.out.println(user);
+        user.getPortfolio().forEach(System.out::println);
+
+        //Показать портфель пользователя
+        String url3 = "http://localhost:8080/user/portfolio/{name}";
+        List<?> portfolio = restTemplate.getForObject(url3, List.class, "Alexander");
+        portfolio.forEach(System.out::println);
+
     }
 
 }
